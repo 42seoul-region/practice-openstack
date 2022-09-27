64024283e65ec9e5c1d2fcf5ea7c538703cf047b
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-09-27 15:59:21 +0900

    Placement 정리. TODO: 네트워크에서 호스트 드라이버를 쓰기 위해서 구조 고민이 필요함.

--- glance/entrypoint-glance.sh
+++ glance/entrypoint-glance.sh
@@ -19,26 +19,26 @@ openstack_make_service_credential() {
   export OS_AUTH_URL=${KEYSTONE_INTERNAL_ENDPOINT}/v3
   export OS_INTERFACE=internal
 
-  echo "Create Service Project"
+  echo "Create Service Project If Absent . . ."
   openstack project create --or-show --domain default --description "Service Project" ${OPENSTACK_SERVICE_PROJECT}
 
-  echo "Create Glance User"
+  echo "Create Glance User . . ."
   openstack user delete ${GLANCE_USER} || true
   openstack user create --domain default --password ${GLANCE_PASS} ${GLANCE_USER}
 
   echo "Check Glance Role"
   if [ $(openstack role assignment list --names | grep ${GLANCE_USER} | wc -l) -eq 0 ]; then
-    echo "Create Glance Role. . . "
+    echo "Create Glance Role . . ."
     openstack role add --project service --user ${GLANCE_USER} ${OPENSTACK_ADMIN_ROLE}
   fi
 
-  echo "Create Glance Service"
+  echo "Create Glance Service . . ."
   openstack service delete image || true
   openstack service create --name glance --description "OpenStack Image Service" image
 
   echo "Check Glance Endpoint"
   if [ $(openstack endpoint list --service image | grep image | wc -l) -eq 0 ]; then
-    echo "Create Glance Endpoint. . . "
+    echo "Create Glance Endpoint . . ."
     openstack endpoint create --region ${REGION_ID} image public ${GLANCE_PUBLIC_ENDPOINT}
     openstack endpoint create --region ${REGION_ID} image internal ${GLANCE_INTERNAL_ENDPOINT}
     openstack endpoint create --region ${REGION_ID} image admin ${GLANCE_ADMIN_ENDPOINT}
