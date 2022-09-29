8f24b4adcbcb592869132f257eb86812f8a8f86a
Tensiya <40755203+T2SU@users.noreply.github.com>
2022-09-29 17:17:31 +0900

    이건 100% 충돌인데?

--- nova/config-nova.py
+++ nova/config-nova.py
@@ -3,51 +3,67 @@
 import os
 import configparser
 
-config = configparser.ConfigParser()
-config.read('/etc/nova/nova.conf')
+conf_nova = configparser.ConfigParser()
+conf_nova.read('/etc/nova/nova.conf')
 
-config['api_database']['connection'] = 'mysql+pymysql://{NOVA_DATABASE_USER}:{NOVA_DATABASE_PASSWORD}@{NOVA_DATABASE_HOST}:{NOVA_DATABASE_PORT}/{NOVA_API_DATABASE_SCHEME}'.format(**os.environ)
-config['database']['connection'] = 'mysql+pymysql://{NOVA_DATABASE_USER}:{NOVA_DATABASE_PASSWORD}@{NOVA_DATABASE_HOST}:{NOVA_DATABASE_PORT}/{NOVA_DATABASE_SCHEME}'.format(**os.environ)
+# [api_database]
+conf_nova['api_database']['connection'] = 'mysql+pymysql://{NOVA_DATABASE_USER}:{NOVA_DATABASE_PASSWORD}@{NOVA_DATABASE_HOST}:{NOVA_DATABASE_PORT}/{NOVA_API_DATABASE_SCHEME}'.format(**os.environ)
+# [database]
+conf_nova['database']['connection'] = 'mysql+pymysql://{NOVA_DATABASE_USER}:{NOVA_DATABASE_PASSWORD}@{NOVA_DATABASE_HOST}:{NOVA_DATABASE_PORT}/{NOVA_DATABASE_SCHEME}'.format(**os.environ)
 
-# config['DEFAULT']['transport_url'] = 'rabbit://openstack:RABBIT_PASS@controller:5672/'.format(**os.environ)\
-config['DEFAULT']['my_ip'] = os.environ['NOVA_EXTERNAL_HOST']
+# [DEFAULT]
+conf_nova['DEFAULT']['debug'] = os.environ['OPENSTACK_DEBUG']
+# see also transport_url
+# conf_nova['DEFAULT']['transport_url'] = 'rabbit://{RABBIT_USER}:{RABBIT_PASS}@{RABBIT_HOST}/'.format(**os.environ)\
+conf_nova['DEFAULT']['my_ip'] = os.environ['NOVA_EXTERNAL_HOST']
 
-config['api']['auth_strategy'] = 'keystone'
-config['keystone_authtoken']['www_authenticate_uri'] = '{KEYSTONE_PUBLIC_ENDPOINT}'.format(**os.environ)
-config['keystone_authtoken']['auth_url'] = '{KEYSTONE_INTERNAL_ENDPOINT}'.format(**os.environ)
-config['keystone_authtoken']['auth_type'] = 'password'
-config['keystone_authtoken']['project_domain_name'] = 'Default'
-config['keystone_authtoken']['user_domain_name'] = 'Default'
-config['keystone_authtoken']['project_name'] = 'service'
-config['keystone_authtoken']['username'] = os.environ['NOVA_USER']
-config['keystone_authtoken']['password'] = os.environ['NOVA_PASS']
+# [api]
+conf_nova['api']['auth_strategy'] = 'keystone'
 
-config['neutron']['auth_url'] = '{KEYSTONE_INTERNAL_ENDPOINT}'.format(**os.environ)
-config['neutron']['auth_type'] = 'password'
-config['neutron']['project_domain_name'] = 'default'
-config['neutron']['user_domain_name'] = 'default'
-config['neutron']['region_name'] = os.environ['REGION_ID']
-config['neutron']['project_name'] = 'service'
-config['neutron']['username'] = os.environ['NEUTRON_USER']
-config['neutron']['password'] = os.environ['NEUTRON_PASS']
-config['neutron']['metadata_proxy_shared_secret'] = os.environ['METADATA_SECRET']
+# [keystone_authtoken]
+conf_nova['keystone_authtoken']['www_authenticate_uri'] = '{KEYSTONE_PUBLIC_ENDPOINT}'.format(**os.environ)
+conf_nova['keystone_authtoken']['auth_url'] = '{KEYSTONE_INTERNAL_ENDPOINT}'.format(**os.environ)
+# see also memcached_servers
+conf_nova['keystone_authtoken']['auth_type'] = 'password'
+conf_nova['keystone_authtoken']['project_domain_name'] = 'Default'
+conf_nova['keystone_authtoken']['user_domain_name'] = 'Default'
+conf_nova['keystone_authtoken']['project_name'] = 'service'
+conf_nova['keystone_authtoken']['username'] = os.environ['NOVA_USER']
+conf_nova['keystone_authtoken']['password'] = os.environ['NOVA_PASS']
 
-config['vnc']['enabled'] = 'true'
-config['vnc']['server_listen'] = '$my_ip'
-config['vnc']['server_proxyclient_address'] = '$my_ip'
+# [neutron]
+# This section depends on Neutron
+conf_nova['neutron']['auth_url'] = '{KEYSTONE_INTERNAL_ENDPOINT}'.format(**os.environ)
+conf_nova['neutron']['auth_type'] = 'password'
+conf_nova['neutron']['project_domain_name'] = 'default'
+conf_nova['neutron']['user_domain_name'] = 'default'
+conf_nova['neutron']['region_name'] = os.environ['REGION_ID']
+conf_nova['neutron']['project_name'] = 'service'
+conf_nova['neutron']['username'] = os.environ['NEUTRON_USER']
+conf_nova['neutron']['password'] = os.environ['NEUTRON_PASS']
+conf_nova['neutron']['service_metadata_proxy'] = 'true'
+conf_nova['neutron']['metadata_proxy_shared_secret'] = os.environ['METADATA_SECRET']
 
-config['glance']['api_servers'] = '{GLANCE_INTERNAL_ENDPOINT}'.format(**os.environ)
+# [vnc]
+conf_nova['vnc']['enabled'] = 'true'
+conf_nova['vnc']['server_listen'] = '$my_ip'
+conf_nova['vnc']['server_proxyclient_address'] = '$my_ip'
 
-config['oslo_concurrency']['lock_path'] = '/var/lib/nova/tmp'
+# [glance]
+conf_nova['glance']['api_servers'] = '{GLANCE_INTERNAL_ENDPOINT}'.format(**os.environ)
 
-config['placement']['region_name'] = os.environ['REGION_ID']
-config['placement']['project_domain_name'] = 'Default'
-config['placement']['project_name'] = 'service'
-config['placement']['user_domain_name'] = 'Default'
-config['placement']['auth_url'] = '{KEYSTONE_INTERNAL_ENDPOINT}/v3'.format(**os.environ)
-config['placement']['auth_type'] = 'password'
-config['placement']['username'] = os.environ['PLACEMENT_API_USER']
-config['placement']['password'] = os.environ['PLACEMENT_API_PASS']
+# [oslo_concurrency]
+conf_nova['oslo_concurrency']['lock_path'] = '/var/lib/nova/tmp'
 
-with open('/etc/nova/nova.conf', 'w') as configfile:
-    config.write(configfile)
+# [placement]
+conf_nova['placement']['region_name'] = os.environ['REGION_ID']
+conf_nova['placement']['project_domain_name'] = 'Default'
+conf_nova['placement']['project_name'] = 'service'
+conf_nova['placement']['auth_type'] = 'password'
+conf_nova['placement']['user_domain_name'] = 'Default'
+conf_nova['placement']['auth_url'] = '{KEYSTONE_INTERNAL_ENDPOINT}/v3'.format(**os.environ)
+conf_nova['placement']['username'] = os.environ['PLACEMENT_API_USER']
+conf_nova['placement']['password'] = os.environ['PLACEMENT_API_PASS']
+
+with open('/etc/nova/nova.conf', 'w') as f1:
+    conf_nova.write(f1)
---
+++ nova/entrypoint-nova.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "PARAMS"
+echo "$@"
+echo
+
+openstack_make_service_credential() {
+  # Set Openstack Credential
+  export OS_IDENTITY_API_VERSION=3
+  export OS_PROJECT_DOMAIN_ID=default
+  export OS_PROJECT_DOMAIN_NAME=Default
+  export OS_USER_DOMAIN_ID=default
+  export OS_USER_DOMAIN_NAME=Default
+  export OS_PROJECT_NAME=${OPENSTACK_ADMIN_PROJECT}
+  export OS_USERNAME=${KEYSTONE_ADMIN_USER}
+  export OS_PASSWORD=${KEYSTONE_ADMIN_PASS}
+  export OS_AUTH_URL=${KEYSTONE_INTERNAL_ENDPOINT}/v3
+  export OS_INTERFACE=internal
+
+  echo "Create Service Project If Absent . . ."
+  openstack project create --or-show --domain default --description "Service Project" ${OPENSTACK_SERVICE_PROJECT}
+
+  echo "Check Nova User . . ."
+  openstack user delete ${NOVA_USER} || true
+  openstack user create --domain default --password ${NOVA_PASS} ${NOVA_USER}
+
+  echo "Check Nova Role"
+  if [ $(openstack role assignment list --names | grep ${NOVA_USER} | wc -l) -eq 0 ]; then
+    echo "Create Nova Role . . . "
+    openstack role add --project service --user ${NOVA_USER} ${OPENSTACK_ADMIN_ROLE}
+  fi
+
+  echo "Create Nova Service . . ."
+  openstack service delete compute || true
+  openstack service create --name nova --description "OpenStack Image Service" compute
+
+  echo "Check Nova Endpoint"
+  if [ $(openstack endpoint list --service compute | grep compute | wc -l) -eq 0 ]; then
+    echo "Create Nova Endpoint . . . "
+    openstack endpoint create --region ${REGION_ID} compute public ${NOVA_PUBLIC_ENDPOINT}
+    openstack endpoint create --region ${REGION_ID} compute internal ${NOVA_INTERNAL_ENDPOINT}
+    openstack endpoint create --region ${REGION_ID} compute admin ${NOVA_ADMIN_ENDPOINT}
+  fi
+}
+
+configure() {
+  echo "Begin configure nova"
+
+  # Create service credential by OpenStack-Client
+  echo "Run OpenStack-Client in subshell"
+  (
+    echo "BEGIN . . ."
+    openstack_make_service_credential
+    echo "END . . ."
+  )
+
+  # Edit /etc/nova/nova.conf file
+  echo "Edit conf file . . ."
+  ./config.py
+
+  # Management as user 'nova'
+  ## Synchronize nova-api Database
+  ## Register database `cell0`
+  ## Create cell `cell1`
+  ## Synchronize Database
+  ## Finally, Show registered nova cells
+  su -s /bin/sh -c "$(cat << EOF
+nova-manage api_db sync
+nova-manage cell_v2 map_cell0
+nova-manage cell_v2 create_cell --name=cell1 --verbose
+nova-manage db sync
+nova-manage cell_v2 list_cells
+EOF
+)" nova
+
+  echo "Done ! !"
+}
+
+if [ -f /root/.nova_configured ]; then
+  echo "Configure file detected ! !"
+else
+  echo "Configure file not detected . . ."
+  configure
+  touch /root/.nova_configured
+fi
+
+echo
+echo "Starting service . . ."
+
+nova-api &
+nova-scheduler &
+nova-conductor &
+nova-novncproxy &
+
+trap "service_down; exit" SIGKILL
+
+function service_down() {
+  echo "Terminating services..."
+  killall -SIGKILL nova-api
+  killall -SIGKILL nova-scheduler
+  killall -SIGKILL nova-conductor
+  killall -SIGKILL nova-novncproxy
+}
+
+#TODO: 이게 이래버리면 트랩이 유지가 되나?
+exec "$@"
